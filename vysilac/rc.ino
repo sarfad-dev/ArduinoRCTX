397d4ceb0e277598245fbb4f9be8e7db052d13d0
Jakub Holub <60482905+HenyPotter@users.noreply.github.com>
2023-01-26 20:25:23 +0100

    Create rc.ino

---
+++ vysilac/rc.ino
@@ -0,0 +1,37 @@
+  //vysilac
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+#include <RCSwitch.h>
+#include <ArduinoJson.h>
+
+RCSwitch vysilac = RCSwitch();
+
+#define BME280_ADRESA (0x76)
+Adafruit_BME280 bme;
+
+void setup() {
+bme.begin(BME280_ADRESA);
+vysilac.enableTransmit(10);
+vysilac.setProtocol(2);
+Serial.begin(9600);
+}
+
+void loop() {
+
+
+  float temperature = bme.readTemperature();
+  float hummidity = bme.readHumidity();
+  float pressure = bme.readPressure() / 100;
+  vysilac.send(temperature, 16);
+  vysilac.send(hummidity, 32);
+  vysilac.send(pressure, 32);
+  Serial.println(temperature);
+  Serial.println(hummidity);
+  Serial.println(pressure);
+  delay(1000);
+
+
+
+}
