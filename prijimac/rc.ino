6f6ab1d7e8974bf4d6ea1657a43e403b50f53a3e
Jakub Holub <60482905+HenyPotter@users.noreply.github.com>
2023-01-26 21:57:28 +0100

    Create rc.ino

---
+++ prijimac/rc.ino
@@ -0,0 +1,26 @@
+#include <RCSwitch.h>
+
+RCSwitch prijimac = RCSwitch();
+
+void setup() {
+
+  Serial.begin(9600);
+  prijimac.setProtocol(2);
+  prijimac.enableReceive(0);
+}
+
+void loop() {
+  if (prijimac.available()) {
+    int velikost = prijimac.getReceivedValue();
+    if (velikost == 0) {
+
+    } else {
+    delay(500);
+      Serial.print(prijimac.getReceivedValue()) / 10;
+      Serial.print(" ");
+      Serial.print(prijimac.getReceivedBitlength());
+      Serial.println();
+      prijimac.resetAvailable();
+  }
+}
+}
